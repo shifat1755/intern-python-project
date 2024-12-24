28e1ba4fc5d836600b8aa5613abd7713e521af1e
Syed Shifat <SyedShifat.Hasan@optimizely.com>
2024-12-25 03:06:36 +0600

    feat: basic done

--- main.py
+++ main.py
@@ -1,6 +1,75 @@
+from pyjokes import get_joke
+from util import NetworkRequest, retry_with_token_refresh
+from getpass import getpass
+from time import sleep, time
+
+def login():
+    print("Please login to your account..")
+    network_request = NetworkRequest()
+    username = input("Username: ")
+    password = getpass("Password: ")
+    print("loggin in ...")
+    response = network_request.post("/api/auth", {
+        "username": username,
+        "password": password
+    })
+    if response.get('code') == 200:
+        print("log in successful")
+        return response['body']
+    else:
+        print("Login failed:", response.get('error'))
+        return None
+
+@retry_with_token_refresh
+def fetch_tweets(headers):
+    print("checking recent tweets ...")
+    start_time = time()
+    response = NetworkRequest.get("/api/tweets", headers=headers)
+    end_time = time()
+    print(f"(time taken: {(end_time - start_time) * 1000:.4f} ms)")
+    return response
 
 def main():
-    pass
+    user = login()
+    if not user:
+        return
+
+    access_token = user['access_token']
+    refresh_token = user['refresh_token']
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    response = fetch_tweets(headers=headers, refresh_token=refresh_token)
+
+    if response.get('code') != 200:
+        print("Failed to fetch tweets:", response.get('error'))
+        return
+
+    tweets = response['body'][:5]
+    for i, tweet in enumerate(tweets[::-1], start=20):
+        print(f"({tweet['id']}) {tweet['author']['firstname'].title()} tweeted at {tweet['created_at']}:")
+        print(f"{tweet['text']}\n")
+
+    posted_jokes = set()
+    for i in range(2):
+        joke = None
+        while not joke or joke in posted_jokes:
+            joke = get_joke()
+        posted_jokes.add(joke)
+
+        print("posting tweet..")
+        start_time = time()
+        post_response = NetworkRequest.post("/api/tweets", {"text": joke}, headers=headers)
+        end_time = time()
+
+        if post_response.get('code') == 201:
+            print(f"(time taken: {(end_time - start_time) * 1000:.4f} ms)")
+            print("posted tweet. sleeping 1 min now\n")
+        else:
+            print(f"Failed to post tweet {i + 1}: {post_response.get('error')}")
+
+        sleep(60)
 
 if __name__ == '__main__':
     main()
--- util.py
+++ util.py
@@ -1,38 +1,72 @@
 import json
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
-
-"""
-TODO: Write whatever class or function you may need
-but, don't use any third party library
-feel free to make any changes in the given class and its methods' arguments or implementation as you see fit
-"""
-
+from time import sleep, time
 
 class NetworkRequest:
+    baseApi = 'http://127.0.0.1:8000'
+
     @staticmethod
-    def get(url, headers = {}):
-        req = Request(url=url, method='GET')
+    def get(url, headers={}):
+        req = Request(url=NetworkRequest.baseApi + url, method='GET')
         for key, value in headers.items():
             req.add_header(key, value)
-        
-        result = {}
-        with urlopen(req) as res:
-            body = res.read().decode('utf-8')
-            result['body'] = json.loads(body)
-            result['code'] = res.status
-        
+        try:
+            with urlopen(req) as res:
+                body = res.read().decode('utf-8')
+                return {'body': json.loads(body), 'code': res.status}
+        except HTTPError as e:
+            return {'error': e.reason, 'code': e.code}
+
+    @staticmethod
+    def post(url, payload={}, headers={}):
+        req = Request(url=NetworkRequest.baseApi + url, method='POST')
+        for key, value in headers.items():
+            req.add_header(key, value)
+        req.add_header('Content-Type', 'application/json')
+        try:
+            with urlopen(req, data=json.dumps(payload).encode('utf-8')) as res:
+                body = res.read().decode('utf-8')
+                return {'body': json.loads(body), 'code': res.status}
+        except HTTPError as e:
+            return {'error': e.reason, 'code': e.code}
+
+    @staticmethod
+    def put(url, payload={}, headers={}):
+        req = Request(url=NetworkRequest.baseApi + url, method='PUT')
+        for key, value in headers.items():
+            req.add_header(key, value)
+        req.add_header('Content-Type', 'application/json')
+        try:
+            with urlopen(req, data=json.dumps(payload).encode('utf-8')) as res:
+                body = res.read().decode('utf-8')
+                return {'body': json.loads(body), 'code': res.status}
+        except HTTPError as e:
+            return {'error': e.reason, 'code': e.code}
+
+    @staticmethod
+    def delete(url, headers={}):
+        req = Request(url=NetworkRequest.baseApi + url, method='DELETE')
+        for key, value in headers.items():
+            req.add_header(key, value)
+        try:
+            with urlopen(req) as res:
+                body = res.read().decode('utf-8')
+                return {'body': json.loads(body), 'code': res.status}
+        except HTTPError as e:
+            return {'error': e.reason, 'code': e.code}
+
+def retry_with_token_refresh(func):
+    def wrapper(*args, **kwargs):
+        refresh_token = kwargs.pop('refresh_token', None)
+        result = func(*args, **kwargs)
+        if result.get('code') == 401 and refresh_token:
+            print("requesting new access token..")
+            refresh_response = NetworkRequest.post('/api/auth/token', {'refresh_token': refresh_token})
+            if refresh_response.get('code') == 200:
+                print("got new token..\n")
+                new_token = refresh_response['body']['access_token']
+                kwargs['headers']['Authorization'] = f'Bearer {new_token}'
+                return func(*args, **kwargs)
         return result
-    
-    @staticmethod
-    def post():
-        pass
-
-    @staticmethod
-    def put():
-        pass
-
-    @staticmethod
-    def delete():
-        pass
-
+    return wrapper
